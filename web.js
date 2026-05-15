bdee9472e83372008f90f572f491479d76742013
mingchi949-oss <mingchi949@gmail.com>
2026-05-15 22:58:00 +0700

    Add files via upload

---
+++ web.js
@@ -0,0 +1,45 @@
+function toggleSocials() {
+  document.getElementById("socialLinks").classList.toggle("show");
+}
+
+// Close the menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+function buy() {
+  alert("Added to your garage!");
+}
+function showCart() {
+  alert("Your garage is empty.");
+}
+function buy() {
+  window.open("https://t.me/yourusername", "_blank");
+}
+
+function openGallery() {
+  document.getElementById("lightbox").style.display = "flex";
+
+  currentXP += 10;
+
+  document.getElementById("xp-display").innerText = currentXP;
+
+  let percentage = (currentXP / 1000) * 100;
+  document.querySelector(".xp-bar-fill").style.width = percentage + "%";
+}
+function swap(imgSrc) {
+  document.getElementById("currentView").src = imgSrc;
+
+  const main = document.getElementById("currentView");
+  main.style.opacity = 0;
+  setTimeout(() => {
+    main.style.opacity = 1;
+  }, 50);
+}
